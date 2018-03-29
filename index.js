07c22dc1e34970fc0cbc024a5140590bcd1bf8d1
Paweł Koleśnik <pawelkolesnik@wp.pl>
2018-03-29 14:05:14 +0200

    to do

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req,res) {
+	res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+	socket.on('join', function(name){
+		// użytkownika, który pojawił się w aplikacji zapisujemy do serwisu trzymającego listę osób w czacie
+		userService.addUser({
+			id: socket.id,
+			name
+		});
+		// aplikacja emituje zdarzenie update, które aktualizuje informację na temat listy użytkowników każdemu nasłuchującemu na wydarzenie 'update'
+		io.emit('update', {
+			users: userService.getAllUsers()
+		});
+	});
+});
+
+server.listen(3000, function(){
+	console.log('listening on *:3000');
+});
