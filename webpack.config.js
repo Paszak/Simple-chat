2968f8cf22980e4b3737ecfa0fa27109fa72238f
Paweł Koleśnik <pawelkolesnik@wp.pl>
2018-04-05 15:45:44 +0200

    do task

---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeJsPlugin = require('optimize-js-plugin');
+const env = process.env.NODE_ENV || 'development';
+
+
+
+console.log('NODE_ENV:', env);
+module.exports = {
+    entry: [
+            'react-hot-loader/patch',
+         './client/index.js'
+        ],
+            output: {
+                path: path.resolve(__dirname, 'public'),
+                filename: 'app.bundle.js'
+            },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: "babel-loader"
+            },
+            {
+            	test: /\.css$/,
+            	use: [
+	           		{ loader: 'style-loader'},
+	            	{
+	                	loader: 'css-loader',
+	                	options: {
+	                    	modules: true
+	                	}
+	            	}
+	        	]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'client/index.html',
+            filename: 'index.html',
+            inject: 'body'
+        }),
+    ],
+
+    devServer: {
+      proxy: {
+          '/socket.io': {
+              target: 'http://localhost:3000',
+              ws: true
+          }
+      }
+    }
+
+};
+
+if (env === 'production') {
+module.exports.plugins.push(
+    new webpack.optimize.UglifyJsPlugin(),
+    new OptimizeJsPlugin({
+      sourceMap: false
+    })
+  );
+};
+
